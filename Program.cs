3b9d1a9ae11784913c18deb50f39af2ee22efb02
DevSevi <Sverin@WINMACSEVI>
2025-02-25 20:43:41 +0100

    Add classes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp2
+﻿namespace WissemsManagement
 {
     internal class Program
     {
@@ -7,6 +7,114 @@
             Console.WriteLine("Hello, World!");
 
             Console.WriteLine("Test");
+        }
+    }
+
+    [Serializable]
+    abstract class Person
+    {
+        string Name { get; set; }
+        string Vorname { get; set; }
+
+        public Person(string vorname, string name)
+        {
+            Name = name;
+            Vorname = vorname;
+        }
+
+        public string GetNameVorname()
+        {
+            return $"{Vorname} {Name}";
+        }
+    }
+
+    [Serializable]
+    class Projektleiter : Person
+    {
+        public Projektleiter(string vorname, string name) : base(vorname, name)
+        {
+        }
+    }
+
+    [Serializable]
+    class Projektmitarbeiter : Person
+    {
+        public Projektmitarbeiter(string vorname, string name) : base(vorname, name)
+        {
+        }
+    }
+
+    [Serializable]
+    class Projekt
+    {
+        string Name { get; set; }
+        string Kunde { get; set; }
+        string Anforderungen { get; set; }
+
+        public Projekt(string name, string kunde, string anforderungen)
+        {
+            Name = name;
+            Kunde = kunde;
+            Anforderungen = anforderungen;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetKunde()
+        {
+            return Kunde;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetKunde(string kunde)
+        {
+            Kunde = kunde;
+        }
+    }
+
+    [Serializable]
+    abstract class Information
+    {
+        public Information()
+        {
+        }
+        int ID { get; set; }
+        public void Kommentieren()
+        {
+
+        }
+
+        public void Ergaenzen()
+        {
+
+        }
+    }
+
+
+    class Text : Information
+    {
+        public Text(string Titel, string Inhalt)
+        {
+            this.Titel = Titel;
+            this.Inhalt = Inhalt;
+        }
+        string Titel { get; set; }
+        string Inhalt { get; set; }
+        public void Kommentieren()
+        {
+
+        }
+
+        public void Ergaenzen()
+        {
+
         }
     }
 }
